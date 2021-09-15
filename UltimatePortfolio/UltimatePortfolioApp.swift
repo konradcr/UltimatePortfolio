ad2957eb252c8ae396229287ab61ca1917bdfcd8
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-15 17:40:30 +0200

    Initial commit.

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,26 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Konrad Cureau on 14/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
