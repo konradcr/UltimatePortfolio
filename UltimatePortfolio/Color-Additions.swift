96f7a93b25edab7d4f4a1d8fd05b82a1fc7fc1d8
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-17 19:04:10 +0200

    Creating the Home view

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Konrad Cureau on 17/09/2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
