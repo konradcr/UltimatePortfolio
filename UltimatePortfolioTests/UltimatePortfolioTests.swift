806e7d9189a0e8398932755f5c81c3cd04a1aa33
konradcr <63312007+konradcr@users.noreply.github.com>
2021-09-20 18:50:55 +0200

    Testing the basics

---
+++ UltimatePortfolioTests/UltimatePortfolioTests.swift
@@ -0,0 +1,21 @@
+//
+//  UltimatePortfolioTests.swift
+//  UltimatePortfolioTests
+//
+//  Created by Konrad Cureau on 20/09/2021.
+//
+
+import CoreData
+import XCTest
+@testable import UltimatePortfolio
+
+class BaseTestCase: XCTestCase {
+    var dataController: DataController!
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func setUpWithError() throws {
+        dataController = DataController(inMemory: true)
+        managedObjectContext = dataController.container.viewContext
+    }
+
+}
